9960466f4139cd728b4ac9294702e993dd699512
Brent-Kuhn <bkuhn4669@floridapoly.edu>
2019-10-01 00:20:32 -0700

    Making the first commit for lab2

---
+++ lab2/lab2/main.asm
@@ -0,0 +1,44 @@
+;
+; lab2.asm
+;
+; Created: 9/30/2019 7:26:31 AM
+; Author : Brent
+;
+
+
+; Replace with your application code
+start:
+;	Activity 1
+;   LDI R16, $92
+;	LDI R17, $23
+;	LDI R18, $66
+;	LDI R19, $87
+;	LDI R20, $F5
+;	
+;	ADD R16, R17
+;	ADD R16, R18
+;	ADD R16, R19
+;	ADD R16, R20 
+
+;	Activity 2
+;	LDI R20, $AA
+;	LDI R21, $BA
+;	LDI R22, $11
+;	LDI R23, $FF
+;
+;	COM R20
+;	COM R21
+;	COM R22
+;	COM R23
+;
+;	Activity 3
+;	LDI R16, 0b10101010
+;	LABEL1:
+;		OUT PORTB, R16
+;		COM R16
+;	RJMP LABEL1
+
+;	IN R20, PINC
+;	IN R21, PINB
+;	ADD R20, R21
+;	OUT PORTB, R20
